d6902c245f2c962c4083210d7509c9611ffb0216
pcjangir <pcjangir2@gmail.com>
2019-07-27 21:31:32 +0530

    Handle postRequest and donwload session

--- JSONHandler/ViewController.swift
+++ JSONHandler/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 import SwiftyJSON
+import WebKit
 
 class ViewController: UIViewController {
+    
+    let webView: WKWebView  = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -24,9 +31,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
-        fetchPhotos()
         tableView.register(PhotosCell.self, forCellReuseIdentifier: "photos")
         setupViews()
+        downloadPdfFile()
     }
     
     func setupViews() {
@@ -34,12 +41,10 @@ class ViewController: UIViewController {
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[vo]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["vo":tableView]))
     }
     
+    // MARK:- Get request
     
-    
-    func fetchPhotos() {
-        
+    func getRequstTofetchPhotos() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/photos") else { return }
-        
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil { return }
             guard let data = data else { return }
@@ -51,8 +56,96 @@ class ViewController: UIViewController {
                 self.tableView.reloadData()
             }
         }.resume()
+    }
+    
+    // MARK:- Get request with parameters
+    
+    func getRequstTofetchPhotosWith(albumId:String) {
+        let urlString = "https://jsonplaceholder.typicode.com/photos"
         
+        var urlComponet = URLComponents(string: urlString)
+        urlComponet?.queryItems = [URLQueryItem(name: "albumId", value: albumId)]
         
+        guard let url = urlComponet?.url else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil { return }
+            guard let data = data else { return }
+            let photos_ = try? JSONDecoder().decode([Photos].self, from: data)
+            OperationQueue.main.addOperation {
+                if let photos = photos_ {
+                    self.photos = photos
+                }
+                self.tableView.reloadData()
+            }
+        }.resume()
+    }
+    
+    // MARK:- Post request
+    
+    func postRequestTofetchPosts() {
+        let urlString = "https://jsonplaceholder.typicode.com/photos"
+        guard let url = URL(string: urlString) else { return }
+      
+        var request = URLRequest(url: url)
+        request.httpMethod = "post"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let jsonData = try? JSONSerialization.data(withJSONObject: ["album_id" : 4343, "album_name" : "new"], options: [])
+        if let unwrapped_jsonData = jsonData {
+            request.httpBody = unwrapped_jsonData
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error != nil { return }
+            guard let data = data else { return }
+            let json = try? JSON(data: data)
+            if let unwrapped_json = json {
+                print(unwrapped_json)
+            }
+        }.resume()
+    }
+    
+    // MARK:- Download session (PDF File)
+    
+    func downloadPdfFile() {
+        
+        guard let url = URL(string: "http://www.africau.edu/images/default/sample.pdf") else { return }
+        
+        let lasPathComponent = url.lastPathComponent
+        
+        let fileURL = documentDirectoryURL().appendingPathComponent(lasPathComponent)
+    
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            showPDF(filePath: fileURL)
+            return
+        }
+        URLSession.shared.downloadTask(with: url) { (tmp_url, response, error) in
+            if error != nil {
+                return
+            }
+            if let unwrapped_tmp_url = tmp_url {
+                try? FileManager.default.copyItem(at: unwrapped_tmp_url, to: fileURL)
+                DispatchQueue.main.async {
+                    self.showPDF(filePath: fileURL)
+                }
+            }
+        }.resume()
+    }
+    
+    func documentDirectoryURL() -> URL {
+        let doucmentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return doucmentDirectory[0]
+    }
+    
+    func showPDF(filePath: URL) {
+        view.addSubview(webView)
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        webView.load(URLRequest(url: filePath))
     }
 }
 
